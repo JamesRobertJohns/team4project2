b38699cbc0ff2137e3bc44695022d5497278e117
JamesRobertJohns <jyng1503@gmail.com>
2024-10-02 22:47:45 +0800

    implemented graph representations

---
+++ src/project2.hpp
@@ -0,0 +1,80 @@
+#ifndef PROJECT2_HPP
+#define PROJECT2_HPP
+
+class Graph {
+	public:
+		Graph(int v, int e) : numVertex{ v }, numEdge{ e } {}
+		void setNumVertex(int v) { numVertex = v; } 
+		void setNumEdge(int e) { numEdge = e; }
+		int getNumEdge(void) const { return numEdge; }
+		int getNumVertex(void) const { return numVertex; }
+
+	protected:
+		int numVertex, numEdge;
+};
+
+class Graph_List : public Graph {
+	public:
+		Graph_List(int v, int e) : Graph(v, e), adjList(v) {}
+		Graph_List() : Graph(0, 0), adjList(0) {}
+
+		void setNumVertex(int v) { numVertex = v; adjList.resize(v); }
+		void addEdge(int src, int dst, int w) {
+			if (w < 0) { cout << "weight must be positive" << endl; return; }
+			if (src < 0 || src > numVertex || dst < 0 || dst > numVertex) { cout << "index out of bound" << endl; return; }
+			adjList[src].emplace_back(dst, w);				
+			numEdge++;
+		}
+
+		void print(void) const {
+			for (int i = 0; i < numVertex; i ++) {
+				cout << i << ": ";
+				for (const auto& i : adjList[i]) {
+					cout << "(" << i.first << ", " << i.second << ")" << "-->";
+				}
+				cout << endl;
+			}	
+		}
+
+		void setAdjList(vector<vector<pair<int, int>>>& l) { adjList = l; }
+		const vector<vector<pair<int, int>>>& getAdjList() const { return adjList; }
+
+	private:
+		vector<vector<pair<int, int>>> adjList; 
+};
+
+class Graph_Matrix : public Graph {
+	public: 
+		Graph_Matrix(int v, int e) : Graph(v, e), adjMatrix(v, vector<int>(v, INT_MAX)) {}
+		Graph_Matrix() : Graph(0, 0), adjMatrix{0} {}
+
+		void setNumVertex(int v) { 
+			numVertex = v; 
+			adjMatrix.resize(v, vector<int>(v, INT_MAX)); 
+		}
+		void addEdge(int src, int dst, int w) {
+			if (w < 0) {cout << "weight must be positive" << endl; return; }
+			if (src < 0 || src > numVertex || dst < 0 || dst > numVertex) { cout << "index out of bound" << endl; return; }
+			adjMatrix[src][dst] = w;
+			numEdge++;
+		}
+
+		const void print() const {
+			for (int i  = 0; i < numVertex; i++) {
+				for (int j = 0; j < numVertex; j++) {
+					if (adjMatrix[i][j] == INT_MAX) cout << "INF ";
+					else cout << adjMatrix[i][j] << " ";
+				}
+				cout << endl;
+			}
+		}
+
+		void setAdjMatrix(vector<vector<int>>& m) { adjMatrix = m; }
+		const vector<vector<int>>& getAdjMatrix(void) const { return adjMatrix; }
+
+	private:
+		vector<vector<int>> adjMatrix;
+};
+
+
+#endif // PROJECT2_HPP 
---
+++ src/test.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#include "project2.hpp"
+
+int main() {
+	Graph_List a{};
+	a.setNumVertex(5);
+	a.addEdge(0, 1, 10);
+	a.addEdge(0, 2, 20);
+	a.addEdge(1, 2, 10);
+	a.addEdge(2, 1, 10);
+	a.addEdge(1, 4, 30);
+	a.addEdge(2, 3, 20);
+	a.addEdge(3, 5, 10);
+	a.addEdge(3, 4, -1);
+	a.addEdge(6, 5, 10);
+	a.print();
+	cout << "number of vertex " << a.getNumVertex() << endl;
+	cout << "number of edges " << a.getNumEdge() << endl;
+
+	Graph_Matrix b{};
+	b.setNumVertex(5);
+	b.addEdge(0, 1, 10);
+	b.addEdge(0, 2, 20);
+	b.addEdge(1, 2, 10);
+	b.addEdge(2, 1, 10);
+	b.addEdge(1, 4, 30);
+	b.addEdge(2, 3, 20);
+	b.addEdge(3, 5, 10);
+	b.addEdge(3, 4, -1);
+	b.addEdge(6, 5, 10);
+	b.print();
+	cout << "number of vertex " << b.getNumVertex() << endl;
+	cout << "number of edges " << b.getNumEdge() << endl;
+}
